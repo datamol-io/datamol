e3a3a00607d0afe93abad1f38e65028770552d4a
Hadrien Mary <hadrien.mary@gmail.com>
2020-11-23 13:57:38 -0500

    bumped version to 0.2.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="datamol",
-    version="0.2.3",
+    version="0.2.4",
     author="Hadrien Mary",
     author_email="hadrien.mary@gmail.com",
     url="https://github.com/invivoai-platform/datamol",
