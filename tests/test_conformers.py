13e991fe419cf53a74420b3bf963219e4dd2137d
Hadrien Mary <hadrien.mary@gmail.com>
2020-11-25 17:03:58 -0500

    tests

--- tests/test_conformers.py
+++ tests/test_conformers.py
@@ -4,57 +4,63 @@ import pytest
 import datamol as dm
 
 
-class TestConformers(unittest.TestCase):
-    def test_generate(self):
+@pytest.mark.serial
+def test_generate(self):
 
-        with pytest.raises(ValueError):
-            smiles = "CCCC"
-            mol = dm.to_mol(smiles)
-            mol = dm.conformers.generate(mol, method="custom_method")
-
+    with pytest.raises(ValueError):
         smiles = "CCCC"
         mol = dm.to_mol(smiles)
-        mol = dm.conformers.generate(mol, minimize_energy=False)
-        assert mol.GetNumConformers() == 50
+        mol = dm.conformers.generate(mol, method="custom_method")
 
-        conf = mol.GetConformer(0)
-        assert conf.GetPositions().shape == (14, 3)
+    smiles = "CCCC"
+    mol = dm.to_mol(smiles)
+    mol = dm.conformers.generate(mol, minimize_energy=False)
+    assert mol.GetNumConformers() == 50
 
-        # NOTE(hadim): `minimize_energy=True` fails on GA.
-        # props = conf.GetPropsAsDict()
-        # assert "rdkit_uff_energy" in props
+    conf = mol.GetConformer(0)
+    assert conf.GetPositions().shape == (14, 3)
 
-    def test_sasa(self):
+    # NOTE(hadim): `minimize_energy=True` fails on GA.
+    # props = conf.GetPropsAsDict()
+    # assert "rdkit_uff_energy" in props
 
-        with pytest.raises(ValueError):
-            smiles = "O=C(C)Oc1ccccc1C(=O)O"
-            mol = dm.to_mol(smiles)
-            mol = dm.conformers.sasa(mol)
 
-        smiles = "CCCC=O"
+@pytest.mark.serial
+def test_sasa(self):
+
+    with pytest.raises(ValueError):
+        smiles = "O=C(C)Oc1ccccc1C(=O)O"
         mol = dm.to_mol(smiles)
-        mol = dm.conformers.generate(mol, minimize_energy=False)
-        sasa = dm.conformers.sasa(mol)
-        assert sasa.shape == (50,)
+        mol = dm.conformers.sasa(mol)
 
-    def test_rmsd(self):
+    smiles = "CCCC=O"
+    mol = dm.to_mol(smiles)
+    mol = dm.conformers.generate(mol, minimize_energy=False)
+    sasa = dm.conformers.sasa(mol)
+    assert sasa.shape == (50,)
 
-        with pytest.raises(ValueError):
-            smiles = "O=C(C)Oc1ccccc1C(=O)O"
-            mol = dm.to_mol(smiles)
-            mol = dm.conformers.rmsd(mol)
 
-        smiles = "CCCC=O"
+@pytest.mark.serial
+def test_rmsd(self):
+
+    with pytest.raises(ValueError):
+        smiles = "O=C(C)Oc1ccccc1C(=O)O"
         mol = dm.to_mol(smiles)
-        mol = dm.conformers.generate(mol, minimize_energy=False)
-        rmsd = dm.conformers.rmsd(mol)
-        assert rmsd.shape == (50, 50)
+        mol = dm.conformers.rmsd(mol)
 
-    # def test_cluster(self):
-    #     # NOTE(hadim): disable here since something is wrong when running on CI.
-    #     smiles = "O=C(C)Oc1ccccc1C(=O)O"
-    #     mol = dm.to_mol(smiles)
-    #     mol = dm.conformers.generate(mol, n_confs=5, minimize_energy=False)
+    smiles = "CCCC=O"
+    mol = dm.to_mol(smiles)
+    mol = dm.conformers.generate(mol, minimize_energy=False)
+    rmsd = dm.conformers.rmsd(mol)
+    assert rmsd.shape == (50, 50)
 
-    #     clustered_mol = dm.conformers.cluster(mol, return_centroids=False)
-    #     assert len(clustered_mol) == 2
+
+@pytest.mark.serial
+def test_cluster(self):
+    # NOTE(hadim): disable here since something is wrong when running on CI.
+    smiles = "O=C(C)Oc1ccccc1C(=O)O"
+    mol = dm.to_mol(smiles)
+    mol = dm.conformers.generate(mol, n_confs=5, minimize_energy=False)
+
+    clustered_mol = dm.conformers.cluster(mol, return_centroids=False)
+    assert len(clustered_mol) == 2
