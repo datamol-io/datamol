fc31e7fc4b9ee59f2fb122a22888eab4d6ae8be8
Hadrien Mary <hadrien.mary@gmail.com>
2021-02-21 16:06:10 -0500

    more fixes and disable some tests for windows - 2

--- tests/test_log.py
+++ tests/test_log.py
@@ -1,3 +1,5 @@
+import pytest
+
 import datamol as dm
 
 
@@ -15,7 +17,7 @@ def check_logs_are_not_shown(capfd):
     assert err == ""
 
 
-@dm.utils.decorators.disable_on_os("win")
+@pytest.mark.skip_platform("win")
 def test_rdkit_log(capfd):
     """Test multiple rdkit log scenarios."""
 
