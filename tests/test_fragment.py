e2ef35e7c3732a85e7c24b40c4621042955fb47a
Hadrien Mary <hadrien.mary@gmail.com>
2021-02-22 13:20:31 -0500

    add tests for break_mol

--- tests/test_fragment.py
+++ tests/test_fragment.py
@@ -1,57 +1,65 @@
-import unittest
-
-import numpy as np
-
 import datamol as dm
 
 
-class TestFragment(unittest.TestCase):
-    def test_brics(self):
-        smiles = "CCCOCc1cc(c2ncccc2)ccc1"
-        mol = dm.to_mol(smiles)
-        frags = dm.fragment.brics(mol)
-        assert len(frags) == 9
+def test_brics():
+    smiles = "CCCOCc1cc(c2ncccc2)ccc1"
+    mol = dm.to_mol(smiles)
+    frags = dm.fragment.brics(mol)
+    assert len(frags) == 9
 
-    def test_frag(self):
-        smiles = "CCCOCc1cc(c2ncccc2)ccc1"
-        mol = dm.to_mol(smiles)
-        frags = dm.fragment.frag(mol)
-        assert len(frags) == 9
 
-    def test_recap(self):
-        smiles = "CCCOCc1cc(c2ncccc2)ccc1"
-        mol = dm.to_mol(smiles)
-        frags = dm.fragment.recap(mol)
-        assert len(frags) == 3
+def test_frag():
+    smiles = "CCCOCc1cc(c2ncccc2)ccc1"
+    mol = dm.to_mol(smiles)
+    frags = dm.fragment.frag(mol)
+    assert len(frags) == 9
 
-    def test_anybreak(self):
-        smiles = "CCCOCc1cc(c2ncccc2)ccc1"
-        mol = dm.to_mol(smiles)
-        frags = dm.fragment.anybreak(mol)
-        assert len(frags) == 9
 
-    def test_mmpa(self):
-        smiles = "CCCOCc1cc(c2ncccc2)ccc1"
-        mol = dm.to_mol(smiles)
+def test_recap():
+    smiles = "CCCOCc1cc(c2ncccc2)ccc1"
+    mol = dm.to_mol(smiles)
+    frags = dm.fragment.recap(mol)
+    assert len(frags) == 3
 
-        frags = dm.fragment.mmpa_cut(mol)
-        assert len(frags) == 39
-        assert (
-            "CCCOCc1cccc(-c2ccccn2)c1,C(C[*:2])[*:1],C[*:1].c1ccc(-c2cccc(CO[*:2])c2)nc1\n" in frags
-        )
 
-    def test_assemble(self):
+def test_anybreak():
+    smiles = "CCCOCc1cc(c2ncccc2)ccc1"
+    mol = dm.to_mol(smiles)
+    frags = dm.fragment.anybreak(mol)
+    assert len(frags) == 9
 
-        # Fragment a molecule
-        smiles = "CCCOCc1cc(c2ncccc2)ccc1"
-        mol = dm.to_mol(smiles)
-        frags = dm.fragment.brics(mol)
 
-        # Limit the number of fragments to work with because
-        # assembling is computationally intensive.
-        frags = frags[:2]
+def test_mmpa():
+    smiles = "CCCOCc1cc(c2ncccc2)ccc1"
+    mol = dm.to_mol(smiles)
 
-        # Assemble molecules from the list of fragments
-        mols = list(dm.fragment.assemble_fragment_order(frags, max_n_mols=4))
+    frags = dm.fragment.mmpa_cut(mol)
+    assert len(frags) == 39
+    assert "CCCOCc1cccc(-c2ccccn2)c1,C(C[*:2])[*:1],C[*:1].c1ccc(-c2cccc(CO[*:2])c2)nc1\n" in frags
 
-        assert len(mols) == 4
+
+def test_assemble():
+
+    # Fragment a molecule
+    smiles = "CCCOCc1cc(c2ncccc2)ccc1"
+    mol = dm.to_mol(smiles)
+    frags = dm.fragment.brics(mol)
+
+    # Limit the number of fragments to work with because
+    # assembling is computationally intensive.
+    frags = frags[:2]
+
+    # Assemble molecules from the list of fragments
+    mols = list(dm.fragment.assemble_fragment_order(frags, max_n_mols=4))
+
+    assert len(mols) == 4
+
+
+def test_break_mol():
+    smiles = "CCCOCc1cc(c2ncccc2)ccc1"
+    mol = dm.to_mol(smiles)
+    fragments, *_, tree = dm.fragment.break_mol(mol, randomize=False, mode="brics", returnTree=True)
+
+    assert fragments == ["CCC", "O", "C", "c1ccncc1", "c1ccccc1"]
+    assert list(tree.nodes) == [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    assert list(tree.edges) == [(0, 1), (0, 2), (2, 3), (2, 4), (4, 5), (4, 6), (6, 7), (6, 8)]
