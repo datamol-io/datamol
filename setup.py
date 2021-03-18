d0787b1856d88caf995282fd6aaa1c1834356ed8
Hadrien Mary <hadrien.mary@gmail.com>
2021-03-18 15:13:36 -0400

    bumped version to 0.2.9

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setup(
     name="datamol",
-    version="0.2.8",
+    version="0.2.9",
     author="Hadrien Mary",
     author_email="hadrien.mary@gmail.com",
     url="https://github.com/datamol-org/datamol",
