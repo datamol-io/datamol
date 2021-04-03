e610d1a4c119c164aaa21f105ddcad1df7928fb1
Hadrien Mary <hadrien.mary@gmail.com>
2021-04-03 09:43:29 -0400

    start testing the actions module

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,4 +1,18 @@
-def test_me():
+import datamol as dm
 
-    # TODO
-    pass
+
+def test_pick_atom_idx():
+    smiles = "OC1=CC2CCCCC2[N:1]=C1"
+    mol = dm.to_mol(smiles)
+
+    assert isinstance(dm.actions.pick_atom_idx(mol), int)
+    assert dm.actions.pick_atom_idx(mol) <= mol.GetNumAtoms()
+
+
+def test_all_bond_remove():
+
+    smiles = "OC1=CC2CCCCC2[N:1]=C1"
+    mol = dm.to_mol(smiles)
+
+    mols = dm.actions.all_bond_remove(mol)
+    assert isinstance(mols, list)
