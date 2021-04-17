037467db201ac8d836599be17dcba446fa286709
hadim <hadrien.mary@gmail.com>
2021-04-17 13:19:50 -0400

    fixing ga again

---
+++ docs/assets/js/google-analytics.js
@@ -0,0 +1,16 @@
+var gtag_id = "G-0L9PP26N2H";
+
+var script = document.createElement("script");
+script.src = "https://www.googletagmanager.com/gtag/js?id=" + gtag_id;
+document.head.appendChild(script);
+
+location$.subscribe(function (url) {
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  gtag("js", new Date());
+  gtag("config", gtag_id);
+});
---
+++ news/ga.rst
@@ -0,0 +1,23 @@
+**Added:**
+
+* <news item>
+
+**Changed:**
+
+* <news item>
+
+**Deprecated:**
+
+* <news item>
+
+**Removed:**
+
+* <news item>
+
+**Fixed:**
+
+* Fix again and hopefully the last time google analytics.
+
+**Security:**
+
+* <news item>
