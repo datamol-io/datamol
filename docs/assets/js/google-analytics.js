6595946e37d27a27e639d2bf7781ebd14f9ce30b
hadim <hadrien.mary@gmail.com>
2021-04-17 13:38:00 -0400

    fix ga seems to be working

--- docs/assets/js/google-analytics.js
+++ docs/assets/js/google-analytics.js
@@ -4,13 +4,7 @@ var script = document.createElement("script");
 script.src = "https://www.googletagmanager.com/gtag/js?id=" + gtag_id;
 document.head.appendChild(script);
 
-location$.subscribe(function (url) {
-  window.dataLayer = window.dataLayer || [];
-
-  function gtag() {
-    dataLayer.push(arguments);
-  }
-
-  gtag("js", new Date());
-  gtag("config", gtag_id);
-});
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', gtag_id);
