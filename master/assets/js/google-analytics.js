83a75fa3d27f87cb194f0298a90da8ccd3c3adbe
hadim <hadrien.mary@gmail.com>
2021-04-17 12:32:29 -0500

    Deployed 037467d to master with MkDocs 1.1.2 and mike 1.0.0

--- master/assets/js/google-analytics.js
+++ master/assets/js/google-analytics.js
@@ -15,8 +15,9 @@ location$.subscribe(function (url) {
   gtag("config", gtag_id);
 });
 
-
+/*
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', gtag_id);
+*/
