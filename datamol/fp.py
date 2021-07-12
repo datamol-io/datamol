325c35046b12b1b62780dab243c26e8a26d4d2a1
Hadrien Mary <hadrien.mary@gmail.com>
2021-07-12 08:20:50 -0400

    better new fp API

--- datamol/fp.py
+++ datamol/fp.py
@@ -1,6 +1,8 @@
 from typing import Union
 from typing import Optional
 
+import warnings
+
 from rdkit import Chem
 from rdkit.Chem import rdMolDescriptors
 from rdkit.Chem import rdmolops
@@ -38,6 +40,149 @@ _FP_FUNCS = {
     "fcfp-count": rdMolDescriptors.GetHashedMorganFingerprint,
     "topological-count": rdMolDescriptors.GetHashedTopologicalTorsionFingerprint,
     "atompair-count": rdMolDescriptors.GetHashedAtomPairFingerprint,
+}
+
+_FP_DEFAULT_ARGS = {
+    "maccs": {},
+    "avalon": {
+        "nBits": 512,
+        "isQuery": False,
+        "resetVect": False,
+        "bitFlags": pyAvalonTools.avalonSimilarityBits,
+    },
+    # Below is datamol default, not the RDKit default.
+    "ecfp": {
+        "radius": 3,  # ECFP4
+        "nBits": 2048,
+        "invariants": [],
+        "fromAtoms": [],
+        "useChirality": False,
+        "useBondTypes": True,
+        "useFeatures": True,
+    },
+    "fcfp": {
+        "radius": 2,  # FCFP4
+        "nBits": 2048,
+        "invariants": [],  # you may want to provide features invariance
+        "fromAtoms": [],
+        "useChirality": False,
+        "useBondTypes": True,
+        "useFeatures": True,
+    },
+    "topological": {
+        "nBits": 2048,
+        "targetSize": 4,
+        "fromAtoms": 0,
+        "ignoreAtoms": 0,
+        "atomInvariants": 0,
+        "nBitsPerEntry": 4,
+        "includeChirality": False,
+    },
+    "atompair": {
+        "nBits": 2048,
+        "minLength": 1,
+        "maxLength": 30,
+        "fromAtoms": 0,
+        "ignoreAtoms": 0,
+        "atomInvariants": 0,
+        "nBitsPerEntry": 4,
+        "includeChirality": False,
+        "use2D": True,
+        "confId": -1,
+    },
+    "rdkit": {
+        "minPath": 1,
+        "maxPath": 7,
+        "fpSize": 2048,
+        "nBitsPerHash": 2,
+        "useHs": True,
+        "tgtDensity": 0.0,
+        "minSize": 128,
+        "branchedPaths": True,
+        "useBondOrder": True,
+        "atomInvariants": 0,
+        "fromAtoms": 0,
+        "atomBits": None,
+        "bitInfo": None,
+    },
+    "pattern": {"fpSize": 2048, "atomCounts": [], "setOnlyBits": None},
+    "layered": {
+        "fpSize": 2048,
+        "minPath": 1,
+        "maxPath": 7,
+        "atomCounts": [],
+        "setOnlyBits": None,
+        "branchedPaths": True,
+        "fromAtoms": 0,
+    },
+    "secfp": {
+        "n_permutations": 128,
+        "nBits": 2048,
+        "radius": 3,
+        "min_radius": 1,
+        "rings": True,
+        "kekulize": False,
+        "isomeric": False,
+        "seed": 0,
+    },
+    "erg": {"atomTypes": 0, "fuzzIncrement": 0.3, "minPath": 1, "maxPath": 15},
+    "estate": {},
+    # COUNTING FP
+    "ecfp-count": {
+        "radius": 2,  # ECFP4
+        "nBits": 2048,
+        "invariants": [],
+        "fromAtoms": [],
+        "useChirality": False,
+        "useBondTypes": True,
+        "useFeatures": False,
+        "includeRedundantEnvironments": False,
+    },
+    "fcfp-count": {
+        "radius": 2,  # FCFP4
+        "nBits": 2048,
+        "invariants": [],  # you may want to provide features invariance
+        "fromAtoms": [],
+        "useChirality": False,
+        "useBondTypes": True,
+        "useFeatures": True,
+        "includeRedundantEnvironments": False,
+    },
+    "topological-count": {
+        "nBits": 2048,
+        "targetSize": 4,
+        "fromAtoms": 0,
+        "ignoreAtoms": 0,
+        "atomInvariants": 0,
+        "includeChirality": False,
+    },
+    "avalon-count": {
+        "nBits": 512,
+        "isQuery": False,
+        "bitFlags": pyAvalonTools.avalonSimilarityBits,
+    },
+    "rdkit-count": {
+        "minPath": 1,
+        "maxPath": 7,
+        "useHs": True,
+        "branchedPaths": True,
+        "useBondOrder": True,
+        "atomInvariants": 0,
+        "fromAtoms": 0,
+        "atomBits": None,
+        "bitInfo": None,
+    },
+    "atompair-count": {
+        "nBits": 2048,
+        "minLength": 1,
+        "maxLength": 30,
+        "fromAtoms": 0,
+        "ignoreAtoms": 0,
+        "atomInvariants": 0,
+        "includeChirality": False,
+        "use2D": True,
+        "confId": -1,
+    },
 }
 
 
@@ -85,27 +230,18 @@ def fp_to_array(
 
 def to_fp(
     mol: Union[str, Chem.rdchem.Mol],
-    fp_size: int = 2048,
-    radius: int = 3,
-    use_features: bool = True,
-    fp_args: dict = None,
     as_array: bool = True,
     fp_type: str = "ecfp",
+    **fp_args,
 ) -> Optional[Union[np.ndarray, SparseBitVect, ExplicitBitVect, UIntSparseIntVect]]:
     """Compute the molecular fingerprint given a molecule or a SMILES.
 
     Args:
         mol: a molecule or a SMILES.
-        fp_size: Size of morgan fingerprint. Default to 2048.
-            Warning: only apply to the default ECFP fingerprint.
-        radius: Radius of the morgan fingerprints. Default to 3.
-            Warning: only apply to the default ECFP fingerprint.
-        use_features: Whether to use atom features. Default to True.
-            Warning: only apply to the default ECFP fingerprint.
-        fp_args: Arguments to build the fingerprint. Refer to the official RDKit documentation.
         as_array: Whether to return a numpy array of an RDKit vec. Default to True.
         fp_type: The type of the fingerprint. See `dm.list_supported_fingerprints()` for a
             complete list.
+        fp_args: Arguments to build the fingerprint. Refer to the official RDKit documentation.
 
     Returns:
         A fingerprint vector or None
@@ -124,24 +260,29 @@ def to_fp(
     if isinstance(mol, str):
         mol = dm.to_mol(mol)
 
-    if fp_args is None:
-        fp_args = {}
+    # Deal with new API introduced in >=0.4 + throw a warning if needed.
+    if "fp_size" in fp_args:
+        warnings.warn(
+            "Using `fp_size` is now deprecated and will be removed in datamol 0.4.0. Please use `nBits` instead.",
+            DeprecationWarning,
+        )
+        fp_args["nBits"] = fp_args.pop("fp_size")
 
-    # Use the default fp args only for the "ECFP" fingerprint.
-    # NOTE(hadim): not sure what to do here in term of backward compatibility. The current
-    # solution keeps it but leads to an inconsistent API.
-    # NOTE(hadim): also should I use `FP_DEF_PARAMS`? Or let the default being the RDKit ones? I think
-    # I prefer the latter one but some args are required (`radius` for example).
-    if fp_type in ["ecfp", "ecfp-count", "fcfp-count"]:
-        if "fp_size" not in fp_args:
-            fp_args["nBits"] = fp_size
-        if "radius" not in fp_args:
-            fp_args["radius"] = radius
-        if "use_features" not in fp_args:
-            fp_args["useFeatures"] = use_features
+    if "use_features" in fp_args:
+        warnings.warn(
+            "Using `use_features` is now deprecated and will be removed in datamol 0.4.0. Please use `useFeatures` instead.",
+            DeprecationWarning,
+        )
+        fp_args["useFeatures"] = fp_args.pop("use_features")
 
+    # Insert default values.
+    for key, value in _FP_DEFAULT_ARGS[fp_type].items():
+        fp_args.setdefault(key, value)
+
+    # Compute the fingerprint
     fp = fp_func(mol, **fp_args)
 
+    # Convert to a numpy array
     if as_array:
         return fp_to_array(fp)
 
@@ -151,4 +292,4 @@ def to_fp(
 def list_supported_fingerprints():
     """Return the supported fingerprints in datamol."""
 
-    return set(_FP_FUNCS.keys())
+    return _FP_FUNCS
--- tests/test_fp.py
+++ tests/test_fp.py
@@ -1,3 +1,5 @@
+import warnings
+
 import datamol as dm
 
 
@@ -11,7 +13,7 @@ def test_to_fp():
 
 
 def test_list_fp():
-    assert dm.list_supported_fingerprints() == {
+    assert set(dm.list_supported_fingerprints().keys()) == {
         "atompair",
         "atompair-count",
         "avalon-count",
@@ -40,26 +42,47 @@ def test_all_fps():
         print(fp_type)
         args = {}
         args["mol"] = mol
-        args["fp_args"] = dict()
         args["as_array"] = True
         args["fp_type"] = fp_type
         fp = dm.to_fp(**args)
 
         fp_infos[fp_type] = dict(size=len(fp), bits_sum=fp.sum())
 
+    print(fp_infos)
+
     assert fp_infos == {
-        "ecfp-count": {"size": 2048, "bits_sum": 42},
-        "atompair": {"size": 2048, "bits_sum": 68},
-        "layered": {"size": 2048, "bits_sum": 335},
-        "avalon-count": {"size": 512, "bits_sum": 168},
-        "fcfp-count": {"size": 2048, "bits_sum": 42},
-        "pattern": {"size": 2048, "bits_sum": 173},
-        "atompair-count": {"size": 2048, "bits_sum": 78},
         "maccs": {"size": 167, "bits_sum": 21},
-        "rdkit": {"size": 2048, "bits_sum": 354},
-        "estate": {"size": 79, "bits_sum": 13},
-        "topological": {"size": 2048, "bits_sum": 18},
-        "erg": {"size": 315, "bits_sum": 23.4},
         "ecfp": {"size": 2048, "bits_sum": 29},
+        "topological": {"size": 2048, "bits_sum": 18},
+        "atompair": {"size": 2048, "bits_sum": 68},
+        "rdkit": {"size": 2048, "bits_sum": 354},
+        "pattern": {"size": 2048, "bits_sum": 173},
+        "layered": {"size": 2048, "bits_sum": 335},
+        "erg": {"size": 315, "bits_sum": 23.4},
+        "estate": {"size": 79, "bits_sum": 13},
+        "avalon-count": {"size": 512, "bits_sum": 168},
+        "ecfp-count": {"size": 2048, "bits_sum": 35},
+        "fcfp-count": {"size": 2048, "bits_sum": 35},
         "topological-count": {"size": 2048, "bits_sum": 19},
+        "atompair-count": {"size": 2048, "bits_sum": 78},
     }
+
+
+def test_fp_deprecated_args_warnings():
+    smiles = "CC(=O)Oc1ccccc1C(=O)O"
+    mol = dm.to_mol(smiles)
+
+    args = {}
+    args["mol"] = mol
+    args["radius"] = 3
+    args["fp_size"] = 2048
+    args["useFeatures"] = True
+    args["as_array"] = True
+    args["fp_type"] = "ecfp"
+
+    with warnings.catch_warnings(record=True) as w:
+        dm.to_fp(**args)
+
+        assert len(w) == 1
+        assert issubclass(w[-1].category, DeprecationWarning)
+        assert "will be removed in datamol 0.4.0" in str(w[-1].message)
