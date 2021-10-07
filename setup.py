248af8296a47bd4f0046c43fea5d5cba42d37add
Hadrien Mary <hadrien.mary@gmail.com>
2021-10-07 16:25:12 -0400

    bumped version to 0.4.11

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
 
 setup(
     name="datamol",
-    version="0.4.10",
+    version="0.4.11",
     author="Valence Discovery",
     author_email="hadrien@valencediscovery.com",
     url="https://github.com/datamol-org/datamol",
