379250d64a91f4411e850988af79933735cb9c95
Hadrien Mary <hadrien.mary@gmail.com>
2021-12-02 16:42:23 -0500

    propagate more args to mcs and change default

--- datamol/mcs.py
+++ datamol/mcs.py
@@ -4,11 +4,15 @@ from rdkit.Chem import rdFMCS
 
 import datamol as dm
 
+ALLOWED_ATOM_COMPARE = ["CompareAny", "CompareAnyHeavyAtom", "CompareElements", "CompareIsotopes"]
+ALLOWED_BOND_COMPARE = ["CompareAny", "CompareOrder", "CompareOrderExact"]
+ALLOWED_RING_COMPARE = ["IgnoreRingFusion", "PermissiveRingFusion", "StrictRingFusion"]
+
 
 def find_mcs_with_details(
     mols: List[dm.Mol],
     maximize_bonds: bool = True,
-    threshold: float = 1.0,
+    threshold: float = 0.0,
     timeout: int = 5,
     verbose: bool = False,
     match_valences: bool = False,
@@ -16,6 +20,9 @@ def find_mcs_with_details(
     complete_rings_only: bool = False,
     match_chiral_tag: bool = False,
     seed_smarts: str = "",
+    atom_compare: str = "CompareElements",
+    bond_compare: str = "CompareOrder",
+    ring_compare: str = "IgnoreRingFusion",
     **kwargs,
 ):
     """Find the maximum common substructure from a list of molecules.
@@ -31,12 +38,21 @@ def find_mcs_with_details(
         complete_rings_only: Whether to match complete rings only.
         match_chiral_tag: Whether to match chiral tags.
         seed_smarts: The seed SMARTS.
+        atom_compare: One of "CompareAny", "CompareAnyHeavyAtom", "CompareElements",
+            "CompareIsotopes".
+        bond_compare: One of "CompareAny", "CompareOrder", "CompareOrderExact".
+        ring_compare: One of "IgnoreRingFusion", "PermissiveRingFusion", "StrictRingFusion".
         kwargs: Additional arguments for the MCS.
     """
 
-    # NOTE(hadim): eventually propagate enums from
-    # AtomCompare and BondCompare. Details at
-    # https://www.rdkit.org/docs/source/rdkit.Chem.rdFMCS.html
+    if atom_compare not in ALLOWED_ATOM_COMPARE:
+        raise ValueError(f"atom_compare must be one of {ALLOWED_ATOM_COMPARE}")
+
+    if bond_compare not in ALLOWED_BOND_COMPARE:
+        raise ValueError(f"bond_compare must be one of {ALLOWED_BOND_COMPARE}")
+
+    if ring_compare not in ALLOWED_RING_COMPARE:
+        raise ValueError(f"ring_compare must be one of {ALLOWED_RING_COMPARE}")
 
     args = {}
     args["maximizeBonds"] = maximize_bonds
@@ -48,6 +64,10 @@ def find_mcs_with_details(
     args["completeRingsOnly"] = complete_rings_only
     args["matchChiralTag"] = match_chiral_tag
     args["seedSmarts"] = seed_smarts
+    args["atomCompare"] = rdFMCS.AtomCompare.names[atom_compare]
+    args["bondCompare"] = rdFMCS.BondCompare.names[bond_compare]
+    args["ringCompare"] = rdFMCS.RingCompare.names[ring_compare]
+
     args.update(kwargs)
 
     mcs = rdFMCS.FindMCS(mols, **args)
