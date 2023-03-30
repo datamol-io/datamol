88bc15af67a7c045c6ea426ab3a4f350241b058d
Pakman450 <steven.pak10@gmail.com>
2023-03-30 14:27:44 -0400

    feat: complete read_mol2 function

--- datamol/io.py
+++ datamol/io.py
@@ -282,37 +282,44 @@ def read_mol2file(
     fail_if_invalid: bool = False,  
 ) -> List[Mol]:
     
+    """Read a Mol2 File
+
+    Args:
+        urlpath: Path to a file or a file-like object. Path can be remote or local.
+        sanitize: Whether to sanitize the molecules.
+        remove_hs: Whether to remove the existing hydrogens in the SDF files.
+        cleanupSubstructures: Whether to clean up substructure in the Mol2 Files.
+        fail_if_invalid: If set to true, the parser will raise an exception if the molecule is invalid
+            instead of returning None.
+    """
+    
+    block=[]
+    mols=[]
     with fsspec.open(urlpath, compression="infer") as f:
         fReadLines = f.readlines()
+        #reversing due to ambiguous end line for mol2 files
+        fReadLines.reverse()
         for line in fReadLines:
-            # with fsspec.open(urlpath, mode="w") as f:
-            mols = rdmolfiles.MolFromMol2File(
-                urlpath,
-                sanitize=sanitize,
-                removeHs=remove_hs,
-                cleanupSubstructures=cleanupSubstructures
-            )
-
-    return mols
-
-# def read_mol2block(
-#     mol2block: str,
-#     sanitize: bool = True,
-#     cleanupSubstructures: bool = True,
-#     remove_hs: bool = True,
-#     fail_if_invalid: bool = False,  
-# ) -> Optional[dm.Mol]:
+            #ignores any header info
+            if b"#" not in line:
+                block.append(str(line,'utf-8'))
+            #since reversed, this is the 'end' a mol2
+            if b'@<TRIPOS>MOLECULE' in line:
+                block.reverse()
+                mol2block = ",".join(block).replace(',','')
+                mol = rdmolfiles.MolFromMol2Block(
+                    mol2block,
+                    sanitize=sanitize,
+                    removeHs=remove_hs,
+                    cleanupSubstructures=cleanupSubstructures,
+                )
+                if mol is None and fail_if_invalid:
+                    raise ValueError(f"Invalid molecule: {mol2block}")
+                mols.append(mol)
+                block=[]
     
-#     mol = rdmolfiles.MolFromMol2Block(
-#     mol2block,
-#     sanitize=sanitize,
-#     removeHs=remove_hs,
-#     cleanupSubstructures=cleanupSubstructures,
-# )
-#     if mol is None and fail_if_invalid:
-#         raise ValueError(f"Invalid molecule: {mol2block}")
-
-#     return mol
+    mols.reverse()
+    return mols
 
 def read_molblock(
     molblock: str,
