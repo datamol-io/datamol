a29a8bc4f18116f3c6a6bc5a5b32d3ef25c1ab5a
Pakman450 <steven.pak10@gmail.com>
2023-04-04 13:13:34 -0400

    feat: add fn for num of matches for filter catalog

--- datamol/descriptors/descriptors.py
+++ datamol/descriptors/descriptors.py
@@ -7,6 +7,7 @@ from rdkit.Chem import RDConfig
 from rdkit.Chem import Lipinski
 from rdkit.Chem import rdmolops
 from rdkit.Chem import Crippen
+from rdkit.Chem import FilterCatalog
 
 
 from .. import Mol
@@ -68,6 +69,29 @@ n_saturated_carbocycles = rdMolDescriptors.CalcNumSaturatedCarbocycles
 n_saturated_heterocyles = rdMolDescriptors.CalcNumSaturatedHeterocycles
 n_saturated_rings = rdMolDescriptors.CalcNumSaturatedRings
 
+
+
+def n_matches_filt_cat(    
+    mol: Mol,
+    filt_specifier: str = "PAINS",
+) -> int:
+    """Compute the number of PAINS in a molecule.
+
+    Rigid bonds are bonds that are not single and not in rings.
+
+    Args:
+        mol: A molecule.
+        filter_specifier: Specify what kind of filtering catalog you want.
+
+    Returns:
+        n_matches_filter_catalog: number of matches based on specified catalog
+    """
+    PAINS_params = FilterCatalog.FilterCatalogParams()
+    PAINS_params.AddCatalog(FilterCatalog.FilterCatalogParams.FilterCatalogs.names[filt_specifier])
+    PAINS_catalog = FilterCatalog.FilterCatalog(PAINS_params) 
+
+    entries = PAINS_catalog.GetMatches(mol)
+    return len(entries) 
 
 def n_rigid_bonds(mol: Mol) -> int:
     """Compute the number of rigid bonds in a molecule.
