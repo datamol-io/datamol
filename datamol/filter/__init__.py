deeeff0788203c98d79b0e3271170247fd7f6935
Steven Pak <steven.pak10@gmail.com>
2023-04-05 15:44:01 -0400

    feat: add run_filter_catalog fn

--- datamol/filter/__init__.py
+++ datamol/filter/__init__.py
@@ -1,5 +1,6 @@
 
 from .matches import n_matches
 from .matches import get_descriptions
-from .matches import get_filter_set
-
+from .matches import get_props
+from .matches import get_prop_list
+# from .matches import run_filter_catalog
--- datamol/filter/matches.py
+++ datamol/filter/matches.py
@@ -11,6 +11,15 @@ from .._version import is_lower_than_current_rdkit_version
 def set_filter_params(
     filt_specifier
 ) -> object:
+    """Utility function that sets filter set parameters
+
+
+    Args:
+        filter_specifier: Specify what kind of filtering catalog you want.
+
+    Returns:
+        Catalog: set filter set
+    """
     Catalog = FilterCatalog.FilterCatalogParams()
     Catalog.AddCatalog(FilterCatalog.FilterCatalogParams.FilterCatalogs.names[filt_specifier])
     Catalog = FilterCatalog.FilterCatalog(Catalog) 
@@ -33,8 +42,8 @@ def n_matches(
     """
     Catalog = set_filter_params(filt_specifier)
 
-    entries = Catalog.GetMatches(mol)
-    return len(entries)
+    matches = Catalog.GetMatches(mol)
+    return len(matches)
 
 def get_descriptions(    
     mol: Mol,
@@ -54,26 +63,85 @@ def get_descriptions(
 
     entries = Catalog.GetMatches(mol)
     descriptions = [entry.GetDescription() for entry in entries]
+
     return descriptions
 
-def get_filter_set(    
+def get_props(    
     mol: Mol,
     filt_specifier: str = "ALL",
+    prop: str = 'FilterSet'
 ) -> List[str]:
-    """Get the filter set of the RDKIT FilterCatalogs hits in a molecule
-
+    """Get the values of the props RDKit FilterCatalog hits for a molecule
 
     Args:
         mol: A molecule.
         filter_specifier: Specify what kind of filtering catalog you want.
 
     Returns:
-        get_descriptions: list of strings that expresses the filter set of each hit
+        get_props: list of strings that expresses the values of the RDKit FilterCatalog hits
     """
     Catalog = set_filter_params(filt_specifier)
-
     entries = Catalog.GetMatches(mol)
-    props = [entry.GetProp('FilterSet') for entry in entries]
+    vals = [entry.GetProp(prop) for entry in entries]
 
-    return props
+    return vals
 
+def get_prop_list(
+    mol: Mol,
+    filt_specifier: str = "ALL",
+    deconstruct: bool = True,
+) -> List[str]:
+    """Get the list of props of RDKit FilterCatalog hits for a molecule
+
+    Args:
+        mol: A molecule.
+        filter_specifier: Specify what kind of filtering catalog you want.
+        deconstruct: Whether if you want to deconstruct prop list into more readable strings
+
+    Returns:
+        get_props_list: list of all prop keys for each RDKit FilterCatalog hit for a molecule
+    """
+    Catalog = set_filter_params(filt_specifier)
+    entries = Catalog.GetMatches(mol)
+    prop_list = [entry.GetPropList() for entry in entries]
+    if deconstruct:
+        decons_list = []
+        for prop in prop_list:
+            one_hit=[]
+            for single in prop:
+                one_hit.append(single)
+            decons_list.append(one_hit)
+        return decons_list
+    return prop_list
+
+
+
+
+# #
+# def run_filter_catalog(
+#         list_smi: List[str],
+#         filt_specifier: str = "ALL",
+#         num_of_threads: int = 1,
+        
+# ) -> List[str]:
+#     """Run the filter catalog here on a list of smiles to find hits.
+
+#     Args:
+#         mol: A molecule.
+#         filter_specifier: Specify what kind of filtering catalog you want.
+#         deconstruct: Whether if you want to deconstruct prop list into more readable strings
+
+#     Returns:
+#         get_props_list: list of all prop keys for each RDKit FilterCatalog hit for a molecule
+#     """
+#     Catalog = set_filter_params(filt_specifier)
+#     list_of_results = FilterCatalog.RunFilterCatalog(
+#         filterCatalog=Catalog,
+#         smiles=list_smi,
+#         numThreads=num_of_threads,
+#         )
+#     for results in list_of_results:
+#         for line in results:
+#             print(line.GetDescription())
+#     return results
+        
